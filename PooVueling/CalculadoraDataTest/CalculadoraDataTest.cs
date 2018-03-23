8791ce6d113192beafe8455ea91210048fb280f0
Andrea d'Alessandro <dalexandrea@gmail.com>
2018-03-23 10:21:54 +0100

    Adding new DataRow test method

---
+++ PooVueling/CalculadoraDataTest/CalculadoraDataTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDataTest
+{
+    public class CalculadoraDataTest
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [DataRow(2,2,2)]
+        [DataRow(3,3,6)]
+        [DataTestMethod]
+        public void SumaTest(int num1, int num2, int resultado)
+        {
+            Assert.IsTrue(iCalculadora.Suma(num1,num2) == resultado);
+        }
+
+        
+    }
+}
