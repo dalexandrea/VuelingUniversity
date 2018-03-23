c0e89ab1c30ccce997975e0189613a20464aff53
Andrea d'Alessandro <dalexandrea@gmail.com>
2018-03-23 12:00:33 +0100

    Terminal Application

--- PooVueling/PooVueling/Program.cs
+++ PooVueling/PooVueling/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace PooVueling
 {
-    class Program
+    public class Program : Calculadora
     {
         static void Main(string[] args)
-        {
+        {   //guardar eleccion
+            int operation = 0;
+            //guardar el resultado de la eleccion
+            double result = 0;
+
+            //pide number 1
+            Console.WriteLine("Elegir primer numero");
+            string stringFirstNumber = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(stringFirstNumber);
+
+            //pide number 2
+            Console.WriteLine("Elegir segundo numero");
+            string stringSecondNumber = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(stringSecondNumber);
+
+            //lista opciones y preguntar eleccion
+            Console.WriteLine("Elegir entre 1 +,2 -,3 *,4 /,5 exit ");
+            string stringOperation = Console.ReadLine();
+            Int32.TryParse(stringOperation.Text, out operation);
+
+            switch (operation)
+            {
+                case 1:
+                    result = Suma(firstNumber, secondNumber);
+                    break;
+
+                case 2:
+                    result = Resta(firstNumber, secondNumber);
+                    break;
+
+                case 3:
+                    result = Multiplication(firstNumber, secondNumber);
+                    break;
+
+                case 4:
+                    result = Division(firstNumber, secondNumber);
+                    break;
+                case 5:
+                    break;
+
+            }
+            Console.WriteLine("Result" + result);
+            Console.ReadKey();
         }
     }
 }
