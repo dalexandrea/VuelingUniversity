62aa4d8645be60032e56220118712c5cc33c7a8d
Andrea d'Alessandro <dalexandrea@gmail.com>
2018-03-23 13:22:49 +0100

    recreate

--- Students/Students/Program.cs
+++ Students/Students/Program.cs
@@ -2,52 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Students
+namespace Student
 {
     class Student
-     {
+    {
         string ID;
         string Name;
-        String Surname;
-        String DNI;
+        string Surname;
+        string DNI;
+
 
         public void data()
         {
             Console.WriteLine("ID: ");
             ID = Console.ReadLine();
-            Console.WriteLine("");
             Console.WriteLine("Name: ");
             Name = Console.ReadLine();
-            Console.WriteLine("");
             Console.WriteLine("Surname: ");
             Surname = Console.ReadLine();
-            Console.WriteLine("");
             Console.WriteLine("DNI: ");
             DNI = Console.ReadLine();
         }
-  
-        class Program
+        class Student1
         {
+
             static void Main(string[] args)
             {
-                int choice = 0;
-                Console.WriteLine("1 - Crear New Student, 2 - Exit");
-                string stringchoice = Console.ReadLine();
-                Int32.TryParse(stringchoice, out choice);
-
-                switch (choice)
-                { 
-                    case 1:
-
-                        Student S1 = new Student();
-                        S1.data();
-                        break;
-                    case 2:
-                        //create text
-                }
-
+                Student S1 = new Student();
+                S1.data();
             }
         }
     }
