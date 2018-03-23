6484007a619e1a5a39671f0e560582848514cee1
Andrea d'Alessandro <dalexandrea@gmail.com>
2018-03-23 12:37:32 +0100

    Saving Student Data (less the text part)

---
+++ Students/Students/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Student
+     {
+        string ID;
+        string Name;
+        String Surname;
+        String DNI;
+
+        public void data()
+        {
+            Console.WriteLine("ID: ");
+            ID = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Surname: ");
+            Surname = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("DNI: ");
+            DNI = Console.ReadLine();
+        }
+  
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Student S1 = new Student();
+                S1.data();
+
+            }
+        }
+    }
+}
